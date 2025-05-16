22b883286b3daa7d248bfe55aa2609951ecfddde
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-05-16 12:37:35 +0100

    feat: enhance TopNav with wallet connection and styling improvements

--- src/components/top-nav/TopNav.jsx
+++ src/components/top-nav/TopNav.jsx
@@ -1,8 +1,15 @@
 import "./top-nav.scss";
 import { Link } from "react-router-dom";
 import { DashboardIcon, Logo, NavDropDown } from "../../assets/icons/";
+import { useAccount } from "wagmi";
+import { truncateWalletAddress } from "../../lib/truncate-address";
+import { useAccountModal, useConnectModal } from "@rainbow-me/rainbowkit";
 
 const TopNav = () => {
+    const { address, isConnected } = useAccount();
+    const { openAccountModal } = useAccountModal();
+    const { openConnectModal } = useConnectModal();
+
     const NavbarLink = ({ to, children }) => {
             return (
                 <Link to={to}>
@@ -52,20 +59,32 @@ const TopNav = () => {
 
                     <hr />
 
-                    <div>
-                        <span>
-                            <img src="/profile_pic.jpg" alt="" />
-                        </span>
+                    {isConnected ? (
+                        <div onClick={openAccountModal}>
+                            <span>
+                                <img src="/profile_pic.jpg" alt="" />
+                            </span>
 
-                        <div>
-                            <span>Jehee.eth</span>
-                            <span>0x63736hgv393...</span>
+                            <div>
+                                <span>Jehee.eth</span>
+                                {isConnected && (
+                                    <span>
+                                        {truncateWalletAddress(address)}
+                                    </span>
+                                )}
+                            </div>
+
+                            <span>
+                                <NavDropDown />
+                            </span>
                         </div>
-
-                        <span>
-                            <NavDropDown />
-                        </span>
-                    </div>
+                    ) : (
+                        <div className="cta" onClick={openConnectModal}>
+                            <button>
+                                <span>connect</span>
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
         </section>
